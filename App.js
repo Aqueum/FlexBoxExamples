1f579b6050055c2e9bd39c6bdcb1f764825fbbff
Aqueum <martin.currie@aqueum.com>
2018-04-25 17:09:14 +0100

    feat: space-around

--- App.js
+++ App.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react'
-import { StyleSheet, Text, View, AppRegistry } from 'react-native'
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, AppRegistry } from 'react-native';
 
 class FlexboxExamples extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.box}/>
-        <View style={styles.box}/>
-        <View style={styles.box}/>
+        <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.box} />
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'space-around'
   },
   box: {
     height: 50,
     width: 50,
-    backgroundColor: '#e76e63',
-    margin: 10,
+    backgroundColor: '#ef6e63',
+    margin: 10
   }
-})
+});
 
 export default FlexboxExamples;
